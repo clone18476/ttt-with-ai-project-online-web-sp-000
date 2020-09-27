078d951be8bf130f655f130497d70059949ce7b4
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 13:55:24 +0000

    Automatically backed up by Learn

---
+++ lib/players/computer.rb
@@ -0,0 +1,6 @@
+module Players #here we have an overriding module class Players that wraps around the entire class, and then:
+  class Computer < Player  #this 'class Computer' inherits all of the functionality from the Player module that we have 
+    
+  end 
+  
+end 

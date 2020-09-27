a656b7b060d046ffae234bd2e2f1e41a549b0916
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 19:56:14 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,11 @@
 module Players #here we have an overriding module class Players that wraps around the entire class, and then:
   class Computer < Player  #this 'class Computer' inherits all of the functionality from the Player module that we have 
     def move(board)
+      if board.cells[0] == " " || ""
+        "1"
+      
+      
+      
       
     end 
   end 

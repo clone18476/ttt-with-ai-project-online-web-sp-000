420c7a824c83d5b5fd6b5c912073ea52d6bd085f
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 15:38:25 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -13,7 +13,9 @@ class Game
   ]
 
   def initialize(player_1=Players::Human.new("X"), player_2=Players::Human.new("O"), board=Board.new)
-     
+     @player_1 = player_1
+     @player_2 = player_2
+     @board = board
   end 
   
   

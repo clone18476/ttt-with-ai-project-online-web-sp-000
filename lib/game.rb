31ed82fcbd339d0e07fd8fcf99b9af87ec618f55
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 18:48:41 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -34,6 +34,7 @@ class Game
     WIN_COMBINATIONS.detect do |winner|
       @board.cells[winner[0]] == @board.cells[winner[1]] &&
       @board.cells[winner[1]] == @board.cells[winner[2]] 
+    end
      
      
   end 

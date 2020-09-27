884d6b7d756f8fa200b750409af51f1d5971a91d
Cameron Cousins <cameroncousins18@gmail.com>
2020-09-27 19:55:04 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,6 +1,8 @@
 module Players #here we have an overriding module class Players that wraps around the entire class, and then:
   class Computer < Player  #this 'class Computer' inherits all of the functionality from the Player module that we have 
-    
+    def move(board)
+      
+    end 
   end 
   
 end 
